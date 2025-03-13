02cee296facbb76fea436ecc63dda379bcd2ce99
bagindaQuadrant <bpulungan@appen.com>
2025-03-13 22:16:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -464,4 +464,6 @@ app.get('/get_users', authenticateToken, async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () =>
+  console.log(`Server running on port ${PORT}`)
+);
